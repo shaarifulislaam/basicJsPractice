8cb52d6e01f6006fb2d9ae0bcc18673891aa43b6
shaarifulislam7 <sharifulmamun7@gmail.com>
2021-06-23 14:51:24 +0600

    here basic variable practice

---
+++ basic.js
@@ -0,0 +1,22 @@
+var myName= "shariful";
+var earth= "planet";
+var nextEarth ="next planet";
+console.log(earth,myName,nextEarth);
+
+//upercase ,lowercase,camelcase,lowercamelcase
+
+var num1= "12";
+var num1 ="14";
+console.log(num1);
+
+var name ="marlin";
+var myage="14";
+console.log("My Name is ",name,"and my age is",myage,"years.");
+
+//constant >>variable value bar bar change kora jay but constant ar value change kora jay na
+
+const pi ="3.1416";
+
+console.log("value of pi",pi);
+
+
